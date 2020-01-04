0bfad492fba34f41c5cb3b69bef4abf7b11a9dd4
HELMEN Anders <anders.helmen@soprasteria.com>
2020-01-04 01:43:21 +0100

    try selecting stuff from database

--- html/tester.php
+++ html/tester.php
@@ -26,6 +26,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$sql = "SELECT id, beer_name, score FROM results";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["beer_name"]. " " . $row["score"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
 ?>
         <nav class="navbar navbar-inverse navbar-static-top">
                 <div class="container">
