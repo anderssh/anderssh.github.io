71bede400275757e4c2027fee0cfd7f644fd0297
Vetle Fredriksen <vetle.fredriksen@gmail.com>
2020-01-12 01:23:03 +0100

    trying to fix weird <r> error

--- html/tester.php
+++ html/tester.php
@@ -68,13 +68,21 @@ $result = $conn->query($sql);
                   if ($result->num_rows > 0) {
                     // output data of each row
                     while($row = $result->fetch_assoc()) {
-                      echo "<tr>\r\n";
-                      echo "<td>" . $row["beer_name"]. "</td><td>" . $row["score"]. "</td>";
-                      echo "<\tr>";
+                        echo . $row["beer_name"]. . $row["score"]. "<br>";
                     }
-                  } else {
+                } else {
                     echo "0 results";
-                  }
+                }
+                  #if ($result->num_rows > 0) {
+                  #  // output data of each row
+                  #  while($row = $result->fetch_assoc()) {
+                  #    echo "<tr>\r\n";
+                  #    echo "<td>" . $row["beer_name"]. "</td><td>" . $row["score"]. "</td>";
+                  #    echo "<\tr>";
+                  #  }
+                  #} else {
+                  #  echo "0 results";
+                  #}
                   ?>
                 </table>
               </div>
