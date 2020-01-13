23cbd0179790e6108e22f0f5cd6ab7dea771b38f
HELMEN Anders <anders.helmen@soprasteria.com>
2020-01-13 07:30:56 +0100

    huge cleanup and move sorting function

--- html/tester.php
+++ html/tester.php
@@ -28,134 +28,67 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $sql = "SELECT beer_name, score FROM results left join beers on results.beer_id = beers.id";
-
 $result = $conn->query($sql);
-
 ?>
-        <nav class="navbar navbar-inverse navbar-static-top">
-                <div class="container">
-                  <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="/">Ølsmakerorkesteret</a>
-                  </div>
-                  <div id="navbar" class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                      <li><a href="/">Hjem</a></li>
-                      <li class="active"><a href="/html/tester.html">Tester</a></li>
-                      <li><a href="https://github.com/anderssh/anderssh.github.io">Kildekode</a></li>
-                      <li><a href="/html/galleri.html">Galleri</a></li>
-                    </ul>
-                  </div><!--/.nav-collapse -->
-                </div>
-              </nav>
-    <div class="container">
-        <h2 class="ingress">Her finner du alle nedskrevne øltester utført av Ølsmakerorkesteret</h2>
-        <button type="button" class="collapsible">Julen 2018</button>
-          <div class="content">
-            <div class="row">
-              <div class="col-md-8">
-                <table id="myTable">
-                  <tr>
-                    <!--When a header is clicked, run the sortTable function, with a parameter, 0 for sorting by names, 1 for sorting by country:-->
-                    <th onclick="sortTable(0)"><i class="fas fa-beer"></i>Ølnavn</th>
-                    <th onclick="sortTable(1)">Antall lepper</th>
-                  </tr>
-                  <?php
-                  if ($result->num_rows > 0) {
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                      echo "<tr>";
-                      echo "<td>" . $row["beer_name"]. $row["score"]. "</td>";
-                      echo "<\tr>";
-                    }
-                } else {
-                    echo "0 results";
-                }
-                  ?>
-                </table>
-              </div>
-            </div>
-          </div>
-        </button>
-        </h2>
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Ølsmakerorkesteret</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="/">Hjem</a></li>
+            <li class="active"><a href="/html/tester.html">Tester</a></li>
+            <li><a href="https://github.com/anderssh/anderssh.github.io">Kildekode</a></li>
+            <li><a href="/html/galleri.html">Galleri</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
-                <script>
-                function sortTable(n) {
-                  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-                  table = document.getElementById("myTable");
-                  switching = true;
-                  //Set the sorting direction to ascending:
-                  dir = "asc";
-                  /*Make a loop that will continue until
-                  no switching has been done:*/
-                  while (switching) {
-                    //start by saying: no switching is done:
-                    switching = false;
-                    rows = table.rows;
-                    /*Loop through all table rows (except the
-                    first, which contains table headers):*/
-                    for (i = 1; i < (rows.length - 1); i++) {
-                      //start by saying there should be no switching:
-                      shouldSwitch = false;
-                      /*Get the two elements you want to compare,
-                      one from current row and one from the next:*/
-                      x = rows[i].getElementsByTagName("TD")[n];
-                      y = rows[i + 1].getElementsByTagName("TD")[n];
-                      /*check if the two rows should switch place,
-                      based on the direction, asc or desc:*/
-                      if (dir == "asc") {
-                        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                          //if so, mark as a switch and break the loop:
-                          shouldSwitch= true;
-                          break;
-                        }
-                      } else if (dir == "desc") {
-                        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                          //if so, mark as a switch and break the loop:
-                          shouldSwitch = true;
-                          break;
-                        }
-                      }
-                    }
-                    if (shouldSwitch) {
-                      /*If a switch has been marked, make the switch
-                      and mark that a switch has been done:*/
-                      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                      switching = true;
-                      //Each time a switch is done, increase this count by 1:
-                      switchcount ++;
-                    } else {
-                      /*If no switching has been done AND the direction is "asc",
-                      set the direction to "desc" and run the while loop again.*/
-                      if (switchcount == 0 && dir == "asc") {
-                        dir = "desc";
-                        switching = true;
-                      }
-                    }
-                  }
+      <div class="container">
+        <li class="active"><a href="/html/tester.html">Tester</a></li>
+        <li><a href="https://github.com/anderssh/anderssh.github.io">Kildekode</a></li>
+        <li><a href="/html/galleri.html">Galleri</a></li>
+        </div>
+      <div class="container">
+        <li class="active"><a href="/html/tester.html">Tester</a></li>
+        <li><a href="https://github.com/anderssh/anderssh.github.io">Kildekode</a></li>
+        <li><a href="/html/galleri.html">Galleri</a></li>
+      </div>
+    </nav>
+    <div class="container">
+      <h2 class="ingress">Her finner du alle nedskrevne øltester utført av Ølsmakerorkesteret</h2>
+      <button type="button" class="collapsible">Julen 2018</button>
+      <div class="content">
+        <div class="row">
+          <div class="col-md-8">
+            <table id="myTable">
+              <tr>
+                <!--When a header is clicked, run the sortTable function, with a parameter, 0 for sorting by names, 1 for sorting by country:-->
+                <th onclick="sortTable(0)"><i class="fas fa-beer"></i>Ølnavn</th>
+                <th onclick="sortTable(1)">Antall lepper</th>
+              </tr>
+              <?php
+              if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                  echo "<tr>";
+                  echo "<td>" . $row["beer_name"]. "<td></td>". $row["score"]. "</td>";
+                  echo "<\tr>";
                 }
-              </script>
-              <script>
-                var coll = document.getElementsByClassName("collapsible");
-                var i;
-
-                for (i = 0; i < coll.length; i++) {
-                  coll[i].addEventListener("click", function() {
-                    this.classList.toggle("active");
-                    var content = this.nextElementSibling;
-                    if (content.style.display === "block") {
-                      content.style.display = "none";
-                    } else {
-                      content.style.display = "block";
-                    }
-                  });
-                }
-                </script>
-            </div>
+            } else {
+                echo "0 results";
+            }
+              ?>
+            </table>
           </div>
+        </div>
+      </div>
+    </div>
+    <script src="js/sort_table.js" type="text/javascript"></script>
   </body>
