7b07e6124af1932dde9ef678443dfa1218ae0e28
HELMEN Anders <anders.helmen@soprasteria.com>
2020-01-13 06:55:34 +0100

    Use new db structure

--- html/tester.php
+++ html/tester.php
@@ -27,7 +27,8 @@ $conn -> set_charset("utf8");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT id, beer_name, score FROM results";
+$sql = "SELECT id, beer_name, score FROM results left join beers on results.beer_id = beers.id";
+
 $result = $conn->query($sql);
 
 ?>
