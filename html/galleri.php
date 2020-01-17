927a495092571e84fc02fc8263de15666640d56f
Anders Stiksrud Helmen <anders.s.helmen@gmail.com>
2020-01-17 23:26:16 +0100

    Move navbar to file

--- html/galleri.php
+++ html/galleri.php
@@ -28,28 +28,10 @@
   }
   $sql = "SELECT beer_name, image_url FROM beers";
   $result = $conn->query($sql);
+  
+  $page="galleri";
+  include 'navbar.php';
   ?>
-  <nav class="navbar navbar-inverse navbar-static-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="/">Ølsmakerorkesteret</a>
-      </div>
-      <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="/">Hjem</a></li>
-          <li><a href="/html/tester.php">Tester</a></li>
-          <li class="active"><a href="/html/galleri.php">Galleri</a></li>
-          <li><a href="https://github.com/anderssh/anderssh.github.io">Kildekode</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
     <div class="container">
       <h2>Bildegalleri</h2>
       <div class="row">
